af13218748a8dbdc8aabf50c76a0d81dad67aa4f
sturmenta <sturmenta@gmail.com>
2022-06-24 17:55:30 -0300

    monorepo rn crash solved

--- packages/rn-app/metro.config.js
+++ packages/rn-app/metro.config.js
@@ -1,17 +1,25 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+
+const extraNodeModules = {
+  modules: path.resolve(path.join(__dirname, '../../node_modules')),
+};
+
+const watchFolders = [path.resolve(path.join(__dirname, '../../node_modules'))];
+
+const nodeModulesPaths = [path.resolve(path.join(__dirname, './node_modules'))];
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
-        experimentalImportSupport: false,
+        experimentalImportSupport: true,
         inlineRequires: true,
       },
     }),
   },
+  resolver: {
+    extraNodeModules,
+    nodeModulesPaths,
+  },
+  watchFolders,
 };
