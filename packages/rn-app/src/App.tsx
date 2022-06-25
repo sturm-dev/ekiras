de9c9ddeb4e399e3e4f8b9a6211a73af0bb6b579
sturmenta <sturmenta@gmail.com>
2022-06-25 16:38:58 -0300

    moving btt with ethers

--- packages/rn-app/src/App.tsx
+++ packages/rn-app/src/App.tsx
@@ -31,6 +31,7 @@ import {
 import * as ethers from 'ethers';
 import multicallAbi from './multicall.json';
 import {bttcChain} from './bttcChain';
+import {formatBTT} from './formatBTT';
 
 const Section: React.FC<{
   children: React.ReactNode;
@@ -72,39 +73,105 @@ const App = () => {
     tryConnecting();
   }, []);
 
-  const tryConnecting = () => {
+  const tryConnecting = async () => {
+    const address1 = '0xbe921007385971d169a4596ECC175A91f8710a56';
+    const address2 = '0xDdaCAe39c023cc587eAB8AE4E7183670620BE231';
+
     const {rpcUrl, chainId, chainName} = bttcChain;
     const provider = new ethers.providers.StaticJsonRpcProvider(rpcUrl, {
       chainId,
       name: chainName,
     });
 
-    // var wallet = new ethers.Wallet(
-    //   '0x2761906e540d278b6f08eae1b3f7f07b1cc1ac737a8792e8bf0c77e4e3cc5c83',
-    //   provider,
-    // );
+    const privateKey =
+      '0x83bad6e68a6c1dddce3e66c4c84e1f26fc3a142e1dedbcb68e7bdcd1db85324c';
 
-    const contract = new ethers.Contract(
-      '0x365aEf443783331c487Eaf8C576A248f15e221c5',
-      multicallAbi,
-      provider,
-      // wallet,
+    const wallet = new ethers.Wallet(privateKey, provider);
+
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log(
+      '────────────────────────────────────────────────────────────────────────────────',
+    );
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+
+    const contractResult = formatBTT(
+      await new ethers.Contract(
+        '0x365aEf443783331c487Eaf8C576A248f15e221c5',
+        multicallAbi,
+        provider,
+        // wallet,
+      ).getEthBalance(address1),
     );
 
-    const getEthBalancePromise: Promise<any> = contract.getEthBalance(
-      '0xbe921007385971d169a4596ECC175A91f8710a56',
+    console.log(`contractResult`, contractResult, typeof contractResult);
+
+    console.log();
+    console.log('───────────────────────────────────────');
+    console.log();
+
+    const senderBalanceBefore = formatBTT(
+      (await provider.getBalance(address1))._hex,
+    );
+    const receiverBalanceBefore = formatBTT(
+      (await provider.getBalance(address2))._hex,
     );
 
-    getEthBalancePromise
-      .then((tokenBalance: any) => {
-        if (tokenBalance) {
-          const balance = ethers.BigNumber.from(tokenBalance._hex);
-          let amountOfBTT: any = ethers.utils.formatEther(balance);
-          amountOfBTT = Math.round(amountOfBTT * 1e4) / 1e4;
-          console.log(`amountOfBTT`, amountOfBTT, typeof amountOfBTT);
-        }
-      })
-      .catch(e => console.log(`e`, e, typeof e));
+    console.log(
+      `senderBalanceBefore`,
+      senderBalanceBefore,
+      typeof senderBalanceBefore,
+    );
+    console.log(
+      `receiverBalanceBefore`,
+      receiverBalanceBefore,
+      typeof receiverBalanceBefore,
+    );
+
+    const valueToSend = ethers.utils.parseEther('1');
+    const valueToSendInBtt = formatBTT(valueToSend._hex);
+
+    console.log(`valueToSendInBtt`, valueToSendInBtt, typeof valueToSendInBtt);
+
+    const tx = await wallet.sendTransaction({to: address2, value: valueToSend});
+
+    await tx.wait();
+    console.log(`tx`, JSON.stringify(tx, null, 2));
+
+    const senderBalanceAfter = formatBTT(
+      (await provider.getBalance(address1))._hex,
+    );
+    const receiverBalanceAfter = formatBTT(
+      (await provider.getBalance(address2))._hex,
+    );
+
+    console.log(
+      `senderBalanceAfter`,
+      senderBalanceAfter,
+      typeof senderBalanceAfter,
+    );
+    console.log(
+      `receiverBalanceAfter`,
+      receiverBalanceAfter,
+      typeof receiverBalanceAfter,
+    );
+
+    const totalCostOfTxInBtt = senderBalanceBefore - senderBalanceAfter;
+
+    console.log(
+      `totalCostOfTxInBtt`,
+      totalCostOfTxInBtt,
+      typeof totalCostOfTxInBtt,
+    );
   };
 
   return (
