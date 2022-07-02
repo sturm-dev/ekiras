aa803716dbafafe674472b7400a66b34133269ff
sturmenta <sturmenta@gmail.com>
2022-07-02 03:44:08 -0300

    basic logic of contract

--- packages/contracts/src/JustFeedback.sol
+++ packages/contracts/src/JustFeedback.sol
@@ -3,40 +3,32 @@
 pragma solidity ^0.8.9;
 
 contract JustFeedback {
-    address public manager;
-    address payable[] public players;
-
-    constructor() {
-        manager = msg.sender;
+    struct Post {
+        string id;
+        address author;
+        string text;
+        address[] upvotes;
+        address[] downvotes;
     }
 
-    function enter() public payable {
-        require(msg.value > .01 ether);
+    Post[] public posts;
 
-        players.push(payable(msg.sender));
+    function createPost() public {
+        // params ->
+        // - text
     }
 
-    function pseudoRandom() private view returns (uint256) {
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(block.difficulty, block.timestamp, players)
-                )
-            );
+    function getPosts() public view returns (Post[] memory) {
+        // returns ->
+        // - allPosts
     }
 
-    function pickWinner() public onlyManager {
-        uint256 index = pseudoRandom() % players.length;
-        players[index].transfer(address(this).balance);
-        players = new address payable[](0);
-    }
-
-    modifier onlyManager() {
-        require(msg.sender == manager);
-        _;
-    }
-
-    function getPlayers() public view returns (address payable[] memory) {
-        return players;
+    function votePost() public {
+        // author cannot vote on his own post
+        // users can vote on only one post at a time
+        // ─────────────────────────────────────────────────────────────────
+        // params ->
+        // - postId
+        // - upvote
     }
 }
