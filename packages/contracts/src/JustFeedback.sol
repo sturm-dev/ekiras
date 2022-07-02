cfc50f4776c766740c6fc83cedd5ae80d2b32cb9
sturmenta <sturmenta@gmail.com>
2022-07-02 16:09:50 -0300

    smart contract basic - create post - vote post - get post

--- packages/contracts/src/JustFeedback.sol
+++ packages/contracts/src/JustFeedback.sol
@@ -4,31 +4,59 @@ pragma solidity ^0.8.9;
 
 contract JustFeedback {
     struct Post {
-        string id;
         address author;
         string text;
-        address[] upvotes;
-        address[] downvotes;
+        mapping(address => bool) upVotes;
+        uint256 upVotesCount;
+        mapping(address => bool) downVotes;
+        uint256 downVotesCount;
     }
 
-    Post[] public posts;
+    mapping(uint256 => Post) public posts;
+    uint256 postIndex;
 
-    function createPost() public {
-        // params ->
-        // - text
+    constructor() {
+        postIndex = 0;
     }
 
-    function getPosts() public view returns (Post[] memory) {
-        // returns ->
-        // - allPosts
+    function createPost(string memory text) public {
+        posts[postIndex].author = msg.sender;
+        posts[postIndex].text = text;
+        posts[postIndex].upVotesCount = 0;
+        posts[postIndex].downVotesCount = 0;
+
+        postIndex += 1;
     }
 
-    function votePost() public {
-        // author cannot vote on his own post
-        // users can vote on only one post at a time
-        // ─────────────────────────────────────────────────────────────────
-        // params ->
-        // - postId
-        // - upvote
+    function votePost(uint256 postId, bool voteIsTypeUp) public {
+        require(
+            posts[postId].author != 0x0000000000000000000000000000000000000000,
+            "post not created yet"
+        );
+        require(posts[postId].author != msg.sender, "the author cannot vote");
+        require(
+            voteIsTypeUp
+                ? !posts[postId].upVotes[msg.sender]
+                : !posts[postId].downVotes[msg.sender],
+            "only one vote for post"
+        );
+
+        if (voteIsTypeUp) {
+            posts[postId].upVotesCount += 1;
+            posts[postId].upVotes[msg.sender] = true;
+
+            if (posts[postId].downVotes[msg.sender]) {
+                posts[postId].downVotes[msg.sender] = false;
+                posts[postId].downVotesCount -= 1;
+            }
+        } else {
+            posts[postId].downVotesCount += 1;
+            posts[postId].downVotes[msg.sender] = true;
+
+            if (posts[postId].upVotes[msg.sender]) {
+                posts[postId].upVotes[msg.sender] = false;
+                posts[postId].upVotesCount -= 1;
+            }
+        }
     }
 }
