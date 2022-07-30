2d3b14283da8e260bce625caf6d345adc54de79c
sturmenta <sturmenta@gmail.com>
2022-07-30 05:45:48 -0300

    update post when vote

--- packages/subgraph/src/just-feedback.ts
+++ packages/subgraph/src/just-feedback.ts
@@ -1,16 +1,16 @@
-import { CreatePostEvent as CreatePostEventEvent } from "../generated/JustFeedback/JustFeedback";
+import {
+  CreatePostEvent as CreatePostEventEvent,
+  VoteEvent as VoteEventEvent,
+} from "../generated/JustFeedback/JustFeedback";
 import { Post, User } from "../generated/schema";
 
 export function handleCreatePostEvent(event: CreatePostEventEvent): void {
-  let post = Post.load(event.params._id.toString());
+  const post = new Post(event.params._id.toString());
 
-  if (post == null) {
-    post = new Post(event.params._id.toString());
-    post.createdDate = event.params._createdDate;
-    post.text = event.params._text;
-    post.upVotesCount = event.params._upVotesCount;
-    post.downVotesCount = event.params._downVotesCount;
-  }
+  post.createdDate = event.params._createdDate;
+  post.text = event.params._text;
+  post.upVotesCount = event.params._upVotesCount;
+  post.downVotesCount = event.params._downVotesCount;
   post.author = event.params._author.toHexString();
   post.save();
 
@@ -18,5 +18,15 @@ export function handleCreatePostEvent(event: CreatePostEventEvent): void {
   if (!user) {
     user = new User(event.params._author.toHexString());
     user.save();
+  }
+}
+
+export function handleVoteEvent(event: VoteEventEvent): void {
+  let post = Post.load(event.params._postId.toString());
+
+  if (post != null) {
+    post.upVotesCount = event.params._upVotesCount;
+    post.downVotesCount = event.params._downVotesCount;
+    post.save();
   }
 }
