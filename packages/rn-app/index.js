0fc4a3687b4ebc1be4a3713e5da0e9f48af12ab1
sturmenta <sturmenta@gmail.com>
2022-08-05 01:12:11 -0300

    move index logic

--- packages/rn-app/index.js
+++ packages/rn-app/index.js
@@ -1,54 +1,8 @@
-/* eslint-disable react-native/no-inline-styles */
-import React, {useState, useEffect} from 'react';
-import {AppRegistry, ActivityIndicator, View} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-import {persistCache} from 'apollo3-cache-persist';
+import {AppRegistry} from 'react-native';
 
 import {App} from './src';
 import {name as appName} from './app.json';
-import {MyDarkTheme} from '_utils';
 
-const cache = new InMemoryCache();
+AppRegistry.registerComponent(appName, () => App);
 
-const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/sturmenta/just-feedback',
-  cache,
-  defaultOptions: {watchQuery: {fetchPolicy: 'cache-and-network'}},
-});
-
-const _App = () => {
-  const [loadingCache, setLoadingCache] = useState(true);
-
-  useEffect(() => {
-    persistCache({
-      cache,
-      storage: AsyncStorage,
-    }).then(() => setLoadingCache(false));
-  }, []);
-
-  if (loadingCache) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: MyDarkTheme.colors.background,
-        }}>
-        <ActivityIndicator size="large" color={MyDarkTheme.colors.text} />
-      </View>
-    );
-  }
-
-  return (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  );
-};
-
-AppRegistry.registerComponent(appName, () => _App);
-
-// https://thegraph.com/hosted-service/subgraph/sturmenta/just-feedback?selected=playground - test queries
-// https://github.com/GraphQLGuide/guide-react-native/blob/a958f89618be7deed24de9fa990b62549d39bcf1/App.js#L28
+// index logic on /src/index.ts
