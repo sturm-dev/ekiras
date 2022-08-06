2cc9b71dfcb3fd489d0db4389ee30f5391c83cd2
sturmenta <sturmenta@gmail.com>
2022-08-05 22:07:17 -0300

    get gas prices with scrapper

--- packages/rn-app/src/db/read/useGetPolygonGasPrices.ts
+++ packages/rn-app/src/db/read/useGetPolygonGasPrices.ts
@@ -1,5 +1,5 @@
 import {useCallback, useEffect, useState} from 'react';
-// import cheerio from 'cheerio';
+import cheerio from 'cheerio';
 
 export const useGetPolygonGasPrices = ({url}: {url: string}) => {
   const [prices, setPrices] = useState<number[]>();
@@ -8,14 +8,25 @@ export const useGetPolygonGasPrices = ({url}: {url: string}) => {
     fetch(url)
       .then((response: any) => response.text())
       .then((data: any) => {
-        setPrices([1, 2, 3]);
+        let $ = cheerio.load(data);
 
-        console.log(`data`, data);
+        const standard = $('#standardgas').text();
+        const fast = $('#fastgas').text();
+        const rapid = $('#rapidgas').text();
+
+        const formatPrice = (priceInText: string) =>
+          parseFloat(priceInText.replace(/[^0-9.]/g, ''));
+
+        setPrices([
+          formatPrice(standard),
+          formatPrice(fast),
+          formatPrice(rapid),
+        ]);
       });
   }, [url]);
 
   useEffect(() => {
-    getPrices();
+    setInterval(getPrices, 3000);
   }, [getPrices]);
 
   return [prices];
