293ccf32c0e4754f2d34062d6ae5317034bf10a7
sturmenta <sturmenta@gmail.com>
2022-08-19 02:24:54 -0300

    [rn-app] split validate purchase component - to another file

--- packages/rn-app/src/scenes/app/profile/logged-in/my-balance/buy-matic/buy-matic-screen.tsx
+++ packages/rn-app/src/scenes/app/profile/logged-in/my-balance/buy-matic/buy-matic-screen.tsx
@@ -1,27 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, Alert, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, View} from 'react-native';
 import {useNavigation, RouteProp, useTheme} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import IAP, {
-  Product,
-  ProductPurchase,
-  PurchaseError,
-  SubscriptionPurchase,
-} from 'react-native-iap';
 
-import {BackButton, ScreenSafeArea, TextByScale} from '_atoms';
-import {Button, Overlay} from '_molecules';
-import {
-  DEVICE_WIDTH,
-  MyThemeInterfaceColors,
-  secondLog,
-  themedStyleSheet,
-} from '_utils';
+import {BackButton, ScreenSafeArea} from '_atoms';
+import {MyThemeInterfaceColors, themedStyleSheet} from '_utils';
 import {AppStackParamList} from '_navigations';
 
-import {CONSUMABLE_ID, TOKEN_NAME} from 'src/config/constants';
-import {validatePurchaseIos} from './utils/validatePurchaseIos';
 import {estimateTxCosts} from './utils/estimateTxCosts';
+import {ValidatePurchase} from './utils/validate-purchase-component';
 
 export type Screen_BuyMatic__Params = {
   userAddress: string;
@@ -38,114 +25,21 @@ export const Screen_BuyMatic: React.FC<{
   }>;
 }> = ({route}) => {
   const styles = useStyles();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const colors = useTheme().colors as unknown as MyThemeInterfaceColors;
 
   const navigation = useNavigation<Screen_BuyMatic__Prop>();
   const {params} = route;
 
-  const [products, setProducts] = useState<Product[]>([]);
-  const [purchaseLoading, setPurchaseLoading] = useState(false);
-  const [receiveCryptoLoading, setReceiveCryptoLoading] = useState(false);
-  const [purchaseModalLoadingVisible, setPurchaseModalLoadingVisible] =
-    useState(false);
+  React.useEffect(() => {
+    // delete this - is for not showing error of unused vars
+    if (!colors) console.log();
 
-  const validatePurchaseInServer = React.useCallback(
-    async (
-      receipt: string,
-      purchase: ProductPurchase | SubscriptionPurchase,
-    ) => {
-      setPurchaseLoading(false);
-      setReceiveCryptoLoading(true);
-
-      secondLog(`params.userAddress`, params.userAddress);
-      secondLog(`receipt`, !!receipt);
-
-      try {
-        const result = await validatePurchaseIos({
-          userAddress: params.userAddress,
-          receipt,
-        });
-
-        secondLog(`result`, result);
-
-        if (result.status === 'ok') {
-          IAP.finishTransaction(purchase);
-          // TODO: integrate animations and show animation of success when purchase is finished
-        } else {
-          setPurchaseModalLoadingVisible(false);
-          Alert.alert('Error', 'Purchase failed');
-          // TODO: show toast with error
-
-          if (result.status === 'error') {
-            console.error('FAILURE! 💩');
-          } else {
-            throw new Error(`Unknown status: ${result.status}`);
-          }
-        }
-      } catch (e) {
-        console.log('validatePurchaseInServer error', e);
-      }
-
-      setReceiveCryptoLoading(false);
-    },
-    [params.userAddress],
-  );
-
-  const handleIAPPurchaseError = React.useCallback((err: PurchaseError) => {
-    setPurchaseLoading(false);
-    setPurchaseModalLoadingVisible(false);
-
-    console.log(`err - purchaseErrorListener`, JSON.stringify(err, null, 2));
-
-    if (err.code !== 'E_USER_CANCELLED') {
-      Alert.alert(
-        'Error',
-        'Error with your purchase, error code = ' + err.code,
-      );
-    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  React.useEffect(() => {
-    IAP.initConnection()
-      .catch(err => console.log('initConnection error', err))
-      .then(() => {
-        IAP.getProducts([CONSUMABLE_ID])
-          .catch(err => console.log('getProducts error', err))
-          .then(_products => {
-            if (!_products) console.log('no products');
-            else setProducts(_products);
-          });
-
-        IAP.getAvailablePurchases()
-          .catch(err => console.log('getPurchases error', err))
-          .then(async purchases => {
-            if (!purchases || purchases.length === 0)
-              console.log('no purchases');
-            else
-              validatePurchaseInServer(
-                purchases[0].transactionReceipt,
-                purchases[0],
-              );
-          });
-      });
-
-    const purchaseErrorListener = IAP.purchaseErrorListener(
-      handleIAPPurchaseError,
-    );
-
-    const purchaseUpdateListener = IAP.purchaseUpdatedListener(
-      async purchase => {
-        const receipt = purchase.transactionReceipt;
-        if (receipt) validatePurchaseInServer(receipt, purchase);
-      },
-    );
-
-    return () => {
-      purchaseUpdateListener.remove();
-      purchaseErrorListener.remove();
-    };
-  }, [params.userAddress, validatePurchaseInServer, handleIAPPurchaseError]);
+  useEffect(() => {
+    getEstimateTxCosts();
+  }, []);
 
   const getEstimateTxCosts = async () => {
     const {estimatedTxCosts, error} = await estimateTxCosts();
@@ -158,20 +52,7 @@ export const Screen_BuyMatic: React.FC<{
     }
   };
 
-  useEffect(() => {
-    getEstimateTxCosts();
-  }, []);
-
-  const onRequestPurchase = () => {
-    setPurchaseLoading(true);
-    setPurchaseModalLoadingVisible(true);
-    IAP.requestPurchase(CONSUMABLE_ID);
-  };
-
-  // TODO: show modal when making purchase
-  // TODO: hide modal when purchase is finished
-  // TODO: show friendly loading when "verifying purchase in decentralized server to receive the crypto"
-  // TODO: show big message of success when purchase in finished
+  console.log(`params`, params);
 
   // "usdPrice": "0.837069",
   // "gasPrices": {
@@ -190,81 +71,10 @@ export const Screen_BuyMatic: React.FC<{
 
   return (
     <ScreenSafeArea>
-      <BackButton
-        onPress={purchaseLoading ? () => null : () => navigation.goBack()}
-      />
+      <BackButton onPress={() => navigation.goBack()} />
       <View style={styles.container}>
-        {!products || !products.length ? <ActivityIndicator /> : null}
-        {products.map(product => (
-          <>
-            <Button
-              key={product.productId}
-              text={`Buy some ${TOKEN_NAME} for ${product.price} ${product.currency}`}
-              onPress={onRequestPurchase}
-              numberOfLines={2}
-              style={{height: 80}}
-            />
-          </>
-        ))}
+        <ValidatePurchase userAddress={params.userAddress} />
       </View>
-      {purchaseModalLoadingVisible ? (
-        <Overlay isVisible overlayStyle={styles.modal} animationType="fade">
-          <View style={styles.modalContainer}>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                {purchaseLoading ? (
-                  <ActivityIndicator />
-                ) : (
-                  <TextByScale>✅</TextByScale>
-                )}
-              </View>
-              <View style={styles.textContainer}>
-                {purchaseLoading ? (
-                  <TextByScale>
-                    Making the purchase on the Apple Servers...
-                  </TextByScale>
-                ) : (
-                  <TextByScale>
-                    Purchase on the Apple servers complete!
-                  </TextByScale>
-                )}
-              </View>
-            </View>
-            {!purchaseLoading ? (
-              <View style={styles.row}>
-                <View style={styles.iconContainer}>
-                  {receiveCryptoLoading ? (
-                    <ActivityIndicator />
-                  ) : (
-                    <TextByScale>✅</TextByScale>
-                  )}
-                </View>
-                <View style={styles.textContainer}>
-                  {receiveCryptoLoading ? (
-                    <TextByScale>
-                      Validating the Apple purchase on own decentralized
-                      servers...
-                    </TextByScale>
-                  ) : (
-                    <TextByScale>
-                      {`Apple purchase validated & ${TOKEN_NAME} sended to the user! 🎉`}
-                    </TextByScale>
-                  )}
-                </View>
-              </View>
-            ) : null}
-            {!purchaseLoading && !receiveCryptoLoading ? (
-              <View style={{...styles.row, justifyContent: 'center'}}>
-                <Button
-                  disabled={purchaseLoading || receiveCryptoLoading}
-                  text="OK"
-                  onPress={() => setPurchaseModalLoadingVisible(false)}
-                />
-              </View>
-            ) : null}
-          </View>
-        </Overlay>
-      ) : null}
     </ScreenSafeArea>
   );
 };
@@ -275,33 +85,4 @@ const useStyles = themedStyleSheet((colors: MyThemeInterfaceColors) => ({
     flex: 1,
     paddingHorizontal: 30,
   },
-  modal: {
-    flex: 1,
-    padding: 0,
-    borderRadius: 10,
-    backgroundColor: '#fff0',
-  },
-  modalContainer: {
-    flex: 1,
-    width: DEVICE_WIDTH,
-    padding: 20,
-    justifyContent: 'center',
-  },
-  row: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: '100%',
-    padding: 20,
-  },
-  iconContainer: {
-    marginRight: 10,
-  },
-  textContainer: {
-    flex: 1,
-  },
 }));
-
-// sandbox test can be very slow - https://youtu.be/4JLHRV2kiCU?t=2442
-
-// https://www.youtube.com/watch?v=4JLHRV2kiCU&ab_channel=EuanMorgan
-// https://react-native-iap.dooboolab.com/docs/usage_instructions/restoring_purchases
---
+++ packages/rn-app/src/scenes/app/profile/logged-in/my-balance/buy-matic/utils/validate-purchase-component.tsx
@@ -0,0 +1,261 @@
+import React, {useState} from 'react';
+import {useTheme} from '@react-navigation/native';
+
+import IAP, {
+  Product,
+  ProductPurchase,
+  PurchaseError,
+  SubscriptionPurchase,
+} from 'react-native-iap';
+
+import {TextByScale} from '_atoms';
+import {Button, Overlay} from '_molecules';
+import {
+  DEVICE_WIDTH,
+  MyThemeInterfaceColors,
+  secondLog,
+  themedStyleSheet,
+} from '_utils';
+
+import {CONSUMABLE_ID, TOKEN_NAME} from 'src/config/constants';
+import {validatePurchaseIos} from '../utils/validatePurchaseIos';
+import {ActivityIndicator, Alert, View} from 'react-native';
+
+interface ValidatePurchaseProps {
+  userAddress: string;
+}
+
+export const ValidatePurchase: React.FC<ValidatePurchaseProps> = ({
+  userAddress,
+}: ValidatePurchaseProps) => {
+  const styles = useStyles();
+  const colors = useTheme().colors as unknown as MyThemeInterfaceColors;
+
+  const [purchaseLoading, setPurchaseLoading] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [receiveCryptoLoading, setReceiveCryptoLoading] = useState(false);
+  const [purchaseModalLoadingVisible, setPurchaseModalLoadingVisible] =
+    useState(false);
+
+  React.useEffect(() => {
+    // delete this - is for not showing error of unused vars
+    if (!colors) console.log();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const validatePurchaseInServer = React.useCallback(
+    async (
+      receipt: string,
+      purchase: ProductPurchase | SubscriptionPurchase,
+    ) => {
+      setPurchaseLoading(false);
+      setReceiveCryptoLoading(true);
+
+      secondLog(`userAddress`, userAddress);
+      secondLog(`receipt`, !!receipt);
+
+      try {
+        const result = await validatePurchaseIos({
+          userAddress: userAddress,
+          receipt,
+        });
+
+        secondLog(`result`, result);
+
+        if (result.status === 'ok') {
+          IAP.finishTransaction(purchase);
+          // TODO: integrate animations and show animation of success when purchase is finished
+        } else {
+          setPurchaseModalLoadingVisible(false);
+          Alert.alert('Error', 'Purchase failed');
+          // TODO: show toast with error
+
+          if (result.status === 'error') {
+            console.error('FAILURE! 💩');
+          } else {
+            throw new Error(`Unknown status: ${result.status}`);
+          }
+        }
+      } catch (e) {
+        console.log('validatePurchaseInServer error', e);
+      }
+
+      setReceiveCryptoLoading(false);
+    },
+    [userAddress],
+  );
+
+  const handleIAPPurchaseError = React.useCallback((err: PurchaseError) => {
+    setPurchaseLoading(false);
+    setPurchaseModalLoadingVisible(false);
+
+    console.log(`err - purchaseErrorListener`, JSON.stringify(err, null, 2));
+
+    if (err.code !== 'E_USER_CANCELLED') {
+      Alert.alert(
+        'Error',
+        'Error with your purchase, error code = ' + err.code,
+      );
+    }
+  }, []);
+
+  React.useEffect(() => {
+    IAP.initConnection()
+      .catch(err => console.log('initConnection error', err))
+      .then(() => {
+        IAP.getProducts([CONSUMABLE_ID])
+          .catch(err => console.log('getProducts error', err))
+          .then(_products => {
+            if (!_products) console.log('no products');
+            else setProducts(_products);
+          });
+
+        IAP.getAvailablePurchases()
+          .catch(err => console.log('getPurchases error', err))
+          .then(async purchases => {
+            if (!purchases || purchases.length === 0)
+              console.log('no purchases');
+            else
+              validatePurchaseInServer(
+                purchases[0].transactionReceipt,
+                purchases[0],
+              );
+          });
+      });
+
+    const purchaseErrorListener = IAP.purchaseErrorListener(
+      handleIAPPurchaseError,
+    );
+
+    const purchaseUpdateListener = IAP.purchaseUpdatedListener(
+      async purchase => {
+        const receipt = purchase.transactionReceipt;
+        if (receipt) validatePurchaseInServer(receipt, purchase);
+      },
+    );
+
+    return () => {
+      purchaseUpdateListener.remove();
+      purchaseErrorListener.remove();
+    };
+  }, [userAddress, validatePurchaseInServer, handleIAPPurchaseError]);
+
+  const onRequestPurchase = () => {
+    setPurchaseLoading(true);
+    setPurchaseModalLoadingVisible(true);
+    IAP.requestPurchase(CONSUMABLE_ID);
+  };
+
+  // TODO: show modal when making purchase
+  // TODO: hide modal when purchase is finished
+  // TODO: show friendly loading when "verifying purchase in decentralized server to receive the crypto"
+  // TODO: show big message of success when purchase in finished
+
+  return (
+    <>
+      {!products || !products.length ? <ActivityIndicator /> : null}
+      {products.map(product => (
+        <Button
+          key={product.productId}
+          text={`Buy some ${TOKEN_NAME} for ${product.price} ${product.currency}`}
+          onPress={onRequestPurchase}
+          numberOfLines={2}
+          style={{height: 80}}
+        />
+      ))}
+      {/* MODAL WHEN VALIDATING PURCHASE ────────────────────────────────────────────────────────────────────────────────  */}
+      {purchaseModalLoadingVisible ? (
+        <Overlay isVisible overlayStyle={styles.modal} animationType="fade">
+          <View style={styles.modalContainer}>
+            <View style={styles.row}>
+              <View style={styles.iconContainer}>
+                {purchaseLoading ? (
+                  <ActivityIndicator />
+                ) : (
+                  <TextByScale>✅</TextByScale>
+                )}
+              </View>
+              <View style={styles.textContainer}>
+                {purchaseLoading ? (
+                  <TextByScale>
+                    Making the purchase on the Apple Servers...
+                  </TextByScale>
+                ) : (
+                  <TextByScale>
+                    Purchase on the Apple servers complete!
+                  </TextByScale>
+                )}
+              </View>
+            </View>
+            {!purchaseLoading ? (
+              <View style={styles.row}>
+                <View style={styles.iconContainer}>
+                  {receiveCryptoLoading ? (
+                    <ActivityIndicator />
+                  ) : (
+                    <TextByScale>✅</TextByScale>
+                  )}
+                </View>
+                <View style={styles.textContainer}>
+                  {receiveCryptoLoading ? (
+                    <TextByScale>
+                      Validating the Apple purchase on own decentralized
+                      servers...
+                    </TextByScale>
+                  ) : (
+                    <TextByScale>
+                      {`Apple purchase validated & ${TOKEN_NAME} sended to the user! 🎉`}
+                    </TextByScale>
+                  )}
+                </View>
+              </View>
+            ) : null}
+            {!purchaseLoading && !receiveCryptoLoading ? (
+              <View style={{...styles.row, justifyContent: 'center'}}>
+                <Button
+                  disabled={purchaseLoading || receiveCryptoLoading}
+                  text="OK"
+                  onPress={() => setPurchaseModalLoadingVisible(false)}
+                />
+              </View>
+            ) : null}
+          </View>
+        </Overlay>
+      ) : null}
+    </>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useStyles = themedStyleSheet((colors: MyThemeInterfaceColors) => ({
+  modal: {
+    flex: 1,
+    padding: 0,
+    borderRadius: 10,
+    backgroundColor: '#fff0',
+  },
+  modalContainer: {
+    flex: 1,
+    width: DEVICE_WIDTH,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  row: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    padding: 20,
+  },
+  iconContainer: {
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+}));
+
+// sandbox test can be very slow - https://youtu.be/4JLHRV2kiCU?t=2442
+
+// https://www.youtube.com/watch?v=4JLHRV2kiCU&ab_channel=EuanMorgan
+// https://react-native-iap.dooboolab.com/docs/usage_instructions/restoring_purchases
